82acb313370542dde2b8689a31992bb07ecb146d
KokiLee <riko2501@gmail.com>
2024-04-08 13:39:43 +0900

    I turned hwt905_ttl_dataparser into a module and put it in a file.

--- serial_communication_async.py
+++ serial_communication_async.py
@@ -14,6 +14,7 @@ import serial_asyncio
 from matplotlib.animation import FuncAnimation
 
 from constants import ascii_control_codes
+from hwt905_ttl_dataparser import HWT905_TTL_Dataparser
 
 handler = RotatingFileHandler("app.log", maxBytes=6000000, backupCount=5)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -163,99 +164,6 @@ class DataParser:
             except Exception as e:
                 logger.error(f"Fail parity check: {e}")
                 return False
-
-
-# HWT905 TTL センサーのデータ解析専用。角度、磁場データを解析して適切な形式で出力する。
-# 別ファイルにわける。
-class HWT905_TTL_Dataparser:
-    @staticmethod
-    def adjust_angular_async(current_angle, previous_angle):
-        """
-        Corrected overflow and underflow when angular difference exceeding 180 degree
-        :return: corrected angle
-        """
-        if previous_angle != 0:
-            angle_diff = current_angle - previous_angle
-            if angle_diff > 180:
-                current_angle -= 360
-            elif angle_diff < -180:
-                current_angle += 360
-        return current_angle
-
-    @staticmethod
-    def protocol_angular_output(data):
-        previous_roll = 0
-        previous_pitch = 0
-        previous_yaw = 0
-        roll = None
-        pitch = None
-        yaw = None
-        try:
-            for i in range(len(data) - 1):
-                if data[i] == 0x55 and data[i + 1] == 0x53:
-                    # Byte value: An integer representing a single byte. Example: 85 or 0x55
-                    # Byte string: A sequence containing one or more byte values. Example: b"\x55"
-
-                    # Combine as a byte sequence.
-                    roll_L_H = bytes([data[i + 2], data[i + 3]])
-                    combined_roll = int.from_bytes(
-                        roll_L_H, byteorder="little", signed=True
-                    )
-                    roll = combined_roll / 32768.0 * 180
-
-                    pitch_L_H = bytes([data[i + 4], data[i + 5]])
-                    combined_pitch = int.from_bytes(
-                        pitch_L_H, byteorder="little", signed=True
-                    )
-                    pitch = combined_pitch / 32768.0 * 180
-
-                    yaw_L_H = bytes([data[i + 6], data[i + 7]])
-                    combined_yaw = int.from_bytes(
-                        yaw_L_H, byteorder="little", signed=True
-                    )
-                    yaw = combined_yaw / 32768.0 * 180
-
-                    # Implemented a solution to correct overflow and underflow in sensor data processing.
-                    roll = HWT905_TTL_Dataparser.adjust_angular_async(
-                        roll, previous_roll
-                    )
-                    previous_roll = roll
-
-                    pitch = HWT905_TTL_Dataparser.adjust_angular_async(
-                        pitch, previous_pitch
-                    )
-                    previous_pitch = pitch
-
-                    yaw = HWT905_TTL_Dataparser.adjust_angular_async(yaw, previous_yaw)
-                    previous_yaw = yaw
-
-            return roll, pitch, yaw
-
-        except Exception as e:
-            logger.error("Data doesn't match")
-
-    @staticmethod
-    def protocol_magnetic_field_output(data):
-        direction = 0
-        magnetic_strength = 0
-        for i in range(len(data) - 1):
-            if data[i] == 0x55 and data[i + 1] == 0x54:
-                hxl_hxh = bytes([data[i + 2], data[i + 3]])
-                hyl_hyh = bytes([data[i + 4], data[i + 5]])
-                hzl_hzh = bytes([data[i + 6], data[i + 7]])
-
-                combined_x = int.from_bytes(hxl_hxh, byteorder="little", signed=True)
-                combined_y = int.from_bytes(hyl_hyh, byteorder="little", signed=True)
-                combined_z = int.from_bytes(hzl_hzh, byteorder="little", signed=True)
-
-                magnetic_strength = math.sqrt(
-                    combined_x**2 + combined_y**2 + combined_z**2
-                )
-                direction = math.atan2(combined_y, combined_x) * (180 / math.pi)
-                if direction < 0:
-                    direction += 360
-
-        return direction, magnetic_strength
 
 
 # 角度データをグラフにプロットするクラス。
