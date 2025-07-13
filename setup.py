4ed6b2f286a09cd315d7d7bd0d8f94d3676eb699
KokiLee <riko2501@gmail.com>
2025-07-13 16:54:18 +0900

    Install editable

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="py_serial_app_for_witmotion",
+    version="1.0.0",
+    description="Serial communication application with GUI",
+    author="Koki Tsukimoto",
+    author_email="riko2501@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "pyserial",
+        "matplotlib",
+        "numpy",
+        "PyQt5",
+        "qasync",
+        "pytest-asyncio",
+        "pytest",
+    ],
+    python_requires=">=3.9.2",
+)
